997d4c767eb28143a10c13398bf3decd10f172c4
fidan2004 <110298605+fidan2004@users.noreply.github.com>
2022-10-04 10:03:15 +0400

    Add files via upload

---
+++ csharp layihe/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace layihe1.Models
+{
+    public class BaseModel
+    {
+        public string Name { get; set; }
+        public bool SoftDelete { get; set; }
+
+    }
+}
---
+++ csharp layihe/Services/Interfaces/IBranchService.cs
@@ -0,0 +1,15 @@
+﻿using layihe1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace layihe1.Services.Interfaces
+{
+    public interface IBranchService : IBankService<Branch>
+    {
+        public void HireEmployee(string name, string brchname);
+        public void GetProfit(Branch branch, Employee employee);
+        public bool TransferMoney(Branch from, Branch to, int amount);
+        public void TransferEmployee(Branch from, Branch to, Employee employee);
+    }
+}
